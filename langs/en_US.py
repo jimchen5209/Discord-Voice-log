bbe4d118f66779851f852c4f5b57d2a6aea53c65
jimchen5209 <jimchen5209@users.noreply.github.com>
2017-10-16 08:27:25 -0500

    Add files via upload

---
+++ langs/en_US.py
@@ -0,0 +1,13 @@
+{
+    "voice_log":{
+        "joined":"{0} joined {1}",
+        "left":"{0} left {1}",
+        "changed":"{0} changed the voice server from {1} to {2}"
+    },
+    "config":{
+        "exist":"I have already been logging voice state to this channel.",
+        "success": "I will now log voice state to this channel",
+        "langsuccess":"Set language to {0}",
+        "langexist":"The language has already been {0}"
+    }
+}
