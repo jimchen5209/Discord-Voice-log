cbad9e405524f5890d0d0e5c6b86cddea6dcf555
jimchen5209 <jimchen5209@users.noreply.github.com>
2017-10-16 08:23:14 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+{
+    "TOKEN":"MzY5MTM2NDUwOTg3NjIyNDAx.DMUIoQ.teeSN_bvEsbRfZXDXKV-dqMT35o", #Insert your discord bot token here.
+    "Debug" : False #If true,raw debug info will be logged into -debug.log file
+}
