d0c3cef6d93c64155426316412e356256f34fce5
jimchen5209 <jimchen5209@gmail.com>
2022-05-12 14:09:45 +0800

    Not 9 o'clock anymore

--- src/Components/Plugin/Plugins/PlayTime-mmis1000.ts
+++ src/Components/Plugin/Plugins/PlayTime-mmis1000.ts
@@ -5,7 +5,7 @@ import { IVoiceOverwrite } from '../Base/VoiceOverwrite';
 
 export class PlayTime_mmis1000 implements IVoiceOverwrite{
     public pluginName = '殘風報時器';
-    public description = '9 點到了';
+    public description = '11 點到了';
     typeVoiceOverwrite = true;
     private ttsHelper: TTSHelper;
 
@@ -16,10 +16,10 @@ export class PlayTime_mmis1000 implements IVoiceOverwrite{
     public async playVoice(member: Member, type: string): Promise<string | undefined> {
         if (member.id === '275570140794322946' && (type === 'switched_in' || type === 'join')) {
             const nowTime = new Date();
-            if (nowTime.getHours() >=0 && nowTime.getHours() < 18) return;
-            const nineOClock = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate(), 21, 0, 0);
+            if (nowTime.getHours() >=2 && nowTime.getHours() < 18) return;
+            const nineOClock = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate(), 23, 0, 0);
             const time = (nowTime.getTime() - nineOClock.getTime()) / 60000;
-            const text = `9 點 ${(time > 0)? Math.floor(time) : Math.ceil(time)} 分到了`;
+            const text = `11 點 ${(time > 0)? Math.floor(time) : Math.ceil(time)} 分到了`;
             const lang = 'zh_tw';
 
             const file = await this.ttsHelper.getTTSFile(text, lang);
