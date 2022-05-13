e1b5c8af623732848ed6d79f2c3b66fe4bdcbdc2
jimchen5209 <jimchen5209@gmail.com>
2022-05-13 23:31:16 +0800

    Workaround tts

--- src/Components/Plugin/Plugins/PlayTime-mmis1000.ts
+++ src/Components/Plugin/Plugins/PlayTime-mmis1000.ts
@@ -17,9 +17,11 @@ export class PlayTime_mmis1000 implements IVoiceOverwrite{
         if (member.id === '275570140794322946' && (type === 'switched_in' || type === 'join')) {
             const nowTime = new Date();
             if (nowTime.getHours() >=2 && nowTime.getHours() < 18) return;
-            const nineOClock = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate(), 23, 0, 0);
+            const nineOClock = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate(), 11, 0, 0);
             const time = (nowTime.getTime() - nineOClock.getTime()) / 60000;
-            const text = `11 點 ${(time > 0)? Math.floor(time) : Math.ceil(time)} 分到了`;
+            const timeInt = Math.floor(time);
+
+            const text = this.timeString(11, timeInt);
             const lang = 'zh_tw';
 
             const file = await this.ttsHelper.getTTSFile(text, lang);
@@ -27,5 +29,64 @@ export class PlayTime_mmis1000 implements IVoiceOverwrite{
         }
 
         return;
+    }
+
+    private timeString(hour: number, minute: number): string{
+        const hourText = this.numberToString(hour, false);
+        const minuteText = this.numberToString(minute, true);
+        return `${hourText}點${((minute != 0) ? `${minuteText}分` : '')}到了`;
+    }
+
+    private numberToString(number: number, addZero: boolean): string{
+        let text = (number < 0) ? '負' : '';
+        const parseNumber = Math.abs(number);
+
+        const splittedText = parseNumber.toString().split('').reverse();
+
+        if (parseNumber >= 1000) {
+            text += `${this.digitToString(splittedText[3])}千`;
+        }
+
+        if (parseNumber >= 100) {
+            text += `${this.digitToString(splittedText[2])}百`;
+        }
+
+        if (parseNumber >= 10) {
+            text += (splittedText[1] != '1') ? `${this.digitToString(splittedText[1])}十` : '十';
+            text += (splittedText[0] != '0') ? `${this.digitToString(splittedText[0])}` : '';
+        } else if (parseNumber == 0) {
+            text += '零';
+        } else {
+            text += `${(addZero ? '零' : '')}${this.digitToString(splittedText[0])}`;
+        }
+        
+        return text;
+    }
+
+    private digitToString(digit: string): string{
+        switch (digit) {
+            case '0':
+                return '零';
+            case '1':
+                return '一';
+            case '2':
+                return '二';
+            case '3':
+                return '三';
+            case '4':
+                return '四';
+            case '5':
+                return '五';
+            case '6':
+                return '六';
+            case '7':
+                return '七';
+            case '8':
+                return '八';
+            case '9':
+                return '九';
+            default:
+                throw Error('digit must between 0 to 9');
+        }
     }
 }
