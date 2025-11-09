04edf60120cdc3ece73a92e930ca8334dc08ff5d
jimchen5209 <jimchen5209@gmail.com>
2025-11-09 13:58:52 +0800

    style: fix lint for PlayTime_mmis1000

--- src/Plugin/Plugins/PlayTime-mmis1000.ts
+++ src/Plugin/Plugins/PlayTime-mmis1000.ts
@@ -1,9 +1,9 @@
-import { Member } from 'eris'
-import { IVoiceOverwrite } from '../Base/VoiceOverwrite'
+import type { Member } from '@projectdysnomia/dysnomia'
 import { instances } from '../../Utils/Instances'
-import { TTSHelper } from '../../Utils/TTSHelper'
+import type { TTSHelper } from '../../Utils/TTSHelper'
+import type { IVoiceOverwrite } from '../Base/VoiceOverwrite'
 
-// eslint-disable-next-line camelcase
+// biome-ignore lint/style/useNamingConvention: username
 export class PlayTime_mmis1000 implements IVoiceOverwrite {
   public pluginName = '殘風報時器'
   public description = '11 點到了'
@@ -17,7 +17,7 @@ export class PlayTime_mmis1000 implements IVoiceOverwrite {
   public async playVoice(member: Member, type: string): Promise<string | undefined> {
     if (member.id === '275570140794322946' && (type === 'switched_in' || type === 'join')) {
       const nowTime = new Date()
-      if (nowTime.getHours() >=2 && nowTime.getHours() < 18) return
+      if (nowTime.getHours() >= 2 && nowTime.getHours() < 18) return
       let day = nowTime.getDate()
       if (nowTime.getHours() >= 0 && nowTime.getHours() < 2) day -= 1
       const nineOClock = new Date(nowTime.getFullYear(), nowTime.getMonth(), day, 23, 0, 0)
@@ -30,18 +30,16 @@ export class PlayTime_mmis1000 implements IVoiceOverwrite {
       const file = await this.ttsHelper.getTTSFile(text, lang)
       if (file && file.length !== 0) return file
     }
-
-
   }
 
   private timeString(hour: number, minute: number): string {
     const hourText = this.numberToString(hour, false)
     const minuteText = this.numberToString(minute, true)
-    return `${hourText}點${((minute !== 0) ? `${minuteText}分` : '')}到了`
+    return `${hourText}點${minute !== 0 ? `${minuteText}分` : ''}到了`
   }
 
   private numberToString(number: number, addZero: boolean): string {
-    let text = (number < 0) ? '負' : ''
+    let text = number < 0 ? '負' : ''
     const parseNumber = Math.abs(number)
 
     const splittedText = parseNumber.toString().split('').reverse()
@@ -55,12 +53,12 @@ export class PlayTime_mmis1000 implements IVoiceOverwrite {
     }
 
     if (parseNumber >= 10) {
-      text += (splittedText[1] !== '1' || parseNumber >= 100) ? `${this.digitToString(splittedText[1])}十` : '十'
-      text += (splittedText[0] !== '0') ? `${this.digitToString(splittedText[0])}` : ''
+      text += splittedText[1] !== '1' || parseNumber >= 100 ? `${this.digitToString(splittedText[1])}十` : '十'
+      text += splittedText[0] !== '0' ? `${this.digitToString(splittedText[0])}` : ''
     } else if (parseNumber === 0) {
       text += '零'
     } else {
-      text += `${(((addZero && number > 0) || parseNumber >= 100) ? '零' : '')}${this.digitToString(splittedText[0])}`
+      text += `${(addZero && number > 0) || parseNumber >= 100 ? '零' : ''}${this.digitToString(splittedText[0])}`
     }
 
     return text
